187c195c66afa0078a1f84027f56c581d067b082
mwhi0023 <141937583+mwhi0023@users.noreply.github.com>
2023-10-08 15:35:32 +1100

    W10 HW

--- Map: HW 9/vega_lite_vis.js
+++ Map: HW 9/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "visualization.vg.json";
+var vg_1 = "visualization.vl.json";
 vegaEmbed("#vis", vg_1).then(function(result) {
 // Access the Vega view instance
 (https://vega.github.io/vega/docs/api/view/) as result.view
